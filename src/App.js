d549232c01a469adb44b9395b4965f809cbb50be
Katlin <katlin.nbanderson@gmail.com>
2019-07-04 16:07:18 -0700

    add Error404 page

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import AdminKegListPage from './AdminKegListPage';
 import AboutPage from './AboutPage';
 import AddKeg from './AddKeg';
 import AdminKeg from './AdminKeg';
+import Error404 from './Error404';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <AdminKeg/>
       <Switch>
         <Route exact path='/' component={SplashPage} />
         <Route path='/about' component={AboutPage} />
@@ -24,6 +24,7 @@ function App() {
         <Route path='/kegs' component={KegListPage} />
         <Route path='/add' component={AddKeg} />
         {/* <Route path='/keg/this' component={KegDetailPage} /> */}
+        <Route component={Error404} />
       </Switch>
       <AdminLink/> 
     </div>
