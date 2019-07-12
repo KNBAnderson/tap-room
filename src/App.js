91c2116663b035c40e3edac62e90d3b44d5b99dc
Katlin <katlin.nbanderson@gmail.com>
2019-07-12 11:34:03 -0700

    update App component for state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Nav from './Nav'
 import AdminLink from './AdminLink'
-// import SplashPage from './SplashPage';
 import KegListPage from './KegListPage';
-// import KegDetailPage from './KegDetailPage';
 import AdminControl from './AdminControl';
 import AboutPage from './AboutPage';
 import AddKeg from './AddKeg';
-// import AdminKeg from './AdminKeg';
 import Error404 from './Error404';
 import Over21Control from './Over21Control';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route exact path='/' component={Over21Control} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/admin' component={AdminControl} />
-        <Route path='/kegs' component={KegListPage} />
-        <Route path='/add' component={AddKeg} />
-        {/* <Route path='/keg/this' component={KegDetailPage} /> */}
-        <Route component={Error404} />
-      </Switch>
-      <AdminLink/> 
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAdminActive: false,
+      currentKeg: null
+    }
+  }
+
+  handleSelectingKeg = (selectedKeg) => {
+    var newCurrentKeg = selectedKeg;
+    this.setState({currentKeg: newCurrentKeg});
+    console.log(newCurrentKeg);
+  }
+
+  handleChangingAdminView = () => {
+    let newAdminActive = !this.state.isAdminActive;
+    this.setState({isAdminActive: newAdminActive})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Over21Control} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/admin' render={() =><AdminControl onChangingAdminView={this.handleChangingAdminView}/>} />
+          <Route path='/kegs' render={() =><KegListPage isAdminActive={this.isAdminActive}/>}/>
+          <Route path='/add' component={AddKeg} />
+          {/* <Route path='/keg' render={() =><KegDetailPage onSelectingKeg={this.handleSelectingKeg}/>} /> */}
+          <Route component={Error404} />
+        </Switch>
+        <AdminLink/> 
+      </div>
+    );
+  }
 }
 
 export default App;
