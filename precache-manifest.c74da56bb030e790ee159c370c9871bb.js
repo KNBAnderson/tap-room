d83578db5d349bfd90237f8ddf1fe3354ed1269e
Katlin <katlin.nbanderson@gmail.com>
2019-07-12 18:20:53 -0700

    Updates

---
+++ precache-manifest.c74da56bb030e790ee159c370c9871bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f991d59bf4c7bbf099ba84e0448fdff",
+    "url": "“http://KNBAnderson.github.io/tap-room/index.html"
+  },
+  {
+    "revision": "9ec9af1cd4459d149aca",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/css/main.863e0bd5.chunk.css"
+  },
+  {
+    "revision": "99d3713d1d9a1836babc",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/js/2.9a3f4877.chunk.js"
+  },
+  {
+    "revision": "9ec9af1cd4459d149aca",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/js/main.67f28e5e.chunk.js"
+  },
+  {
+    "revision": "52dfd6ea4e9e9d5f446a",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/js/runtime~main.2d41315b.js"
+  },
+  {
+    "revision": "24cd138a169ffb5c70f297e478cb7ddc",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/media/AboutBackground.24cd138a.jpg"
+  },
+  {
+    "revision": "0c04ec4bb43661cf08de2e4a627cd612",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/media/SplashBackground.0c04ec4b.jpg"
+  },
+  {
+    "revision": "faa16c38f71bfae5adfc1afff9b5da56",
+    "url": "“http://KNBAnderson.github.io/tap-room/static/media/beer.faa16c38.jpg"
+  }
+]);
